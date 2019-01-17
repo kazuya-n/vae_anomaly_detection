4cc6af641db61fa4af298a5272e10b6f8da97594
kazuya-n <kazuya-1997@asagi.waseda.jp>
2019-01-17 09:33:36 +0000

    first commit

---
+++ cvae_net.py
@@ -0,0 +1,175 @@
+"""
+Copyright Kunihiko Miyoshi, Preferred Networks Inc.
+MIT License
+
+Reference
+https://github.com/pfnet/chainer/blob/master/examples/vae/net.py
+https://github.com/crcrpar/chainer-VAE
+https://github.com/mlosch/CVAE-Torch/blob/master/CVAE.lua
+"""
+
+import six
+
+import chainer
+import chainer.functions as F
+from chainer.functions.loss.vae import gaussian_kl_divergence
+import chainer.links as L
+
+
+class CVAE(chainer.Chain):
+    """Convolutional Variational AutoEncoder"""
+
+    def __init__(self, c_ch, n_ch, z_dim, latent_dim, C=1.0, k=1, wscale=0.02):
+        """
+        Args:
+            args below are for loss function
+            C (int): Usually this is 1.0. Can be changed to control the
+                second term of ELBO bound, which works as regularization.
+            k (int): Number of Monte Carlo samples used in encoded vector.
+            train (bool): If true loss_function is used for training.
+        """
+        """
+        !! Network Requirment!!
+        Source : https://confit.atlas.jp/guide/event-img/jsai2018/2A1-03/public/pdf?type=in
+
+        memo:
+        Convolution2D(self, in_channels, out_channels, ksize=None, stride=1, pad=0, nobias=False, initialW=None, initial_bias=None, *, dilate=1, groups=1)
+        
+        Init channel = Nc
+        Dimension pf z = Nz
+        
+        In Layer n
+        
+        # ksize(Size of kernel) = (4, 4)
+        # stride = 2
+        # channel = Nc * 2**(n-1)
+        # output = batchnormalization
+        # activation = ReLU
+
+        Output Layer
+        
+        # unit = 2 * Nz
+        # # unit of mu = Nz
+        # # unit of ln_sigma_2 = Nz
+
+        """
+        w = chainer.initializers.Normal(scale=wscale)
+        super(CVAE, self).__init__(
+            e_c0=L.Convolution2D(c_ch * 1, n_ch * 1, 4, 2, 1, initialW=w),
+            e_c1=L.Convolution2D(n_ch * 1, n_ch * 2, 4, 2, 1, initialW=w),
+            e_c2=L.Convolution2D(n_ch * 2, n_ch * 4, 4, 2, 1, initialW=w),
+            e_c3=L.Convolution2D(n_ch * 4, n_ch * 8, 4, 2, 1, initialW=w),
+            e_bn0=L.BatchNormalization(n_ch, use_gamma=False),
+            e_bn1=L.BatchNormalization(n_ch * 2, use_gamma=False),
+            e_bn2=L.BatchNormalization(n_ch * 4, use_gamma=False),
+            e_bn3=L.BatchNormalization(n_ch * 8, use_gamma=False),
+            z_mu=L.Linear(n_ch * 8, latent_dim),
+            z_ln_var=L.Linear(n_ch * 8, latent_dim),
+            d_c0=L.Linear(n_ch*8),
+            d_c1=L.Deconvolution2D(n_ch * 8, n_ch * 4, 4, 2, 1, initialW=w),
+            d_c2=L.Deconvolution2D(n_ch * 4, n_ch * 2, 4, 2, 1, initialW=w),
+            d_c3=L.Deconvolution2D(n_ch * 2, n_ch * 1, 4, 2, 1, initialW=w),
+            d_c4=L.Deconvolution2D(n_ch * 1, c_ch * 1, 4, 2, 1, initialW=w),
+            d_x1=L.Deconvolution2D(n_ch, c_ch, 4, 2, 1, initialW=w),
+            d_x2=L.Deconvolution2D(n_ch, c_ch, 4, 2, 1, initialW=w),
+            d_bn0=L.BatchNormalization(n_ch * 4, use_gamma=False),
+            d_bn1=L.BatchNormalization(n_ch * 2, use_gamma=False),
+            d_bn2=L.BatchNormalization(n_ch * 1, use_gamma=False),
+            d_bn3=L.BatchNormalization(c_ch * 1, use_gamma=False),
+            d_bn4=L.BatchNormalization(c_ch * 1, use_gamma=False),
+            d_bnx1=L.BatchNormalization(c_ch, use_gamma=False),
+            d_bnx2=L.BatchNormalization(c_ch, use_gamma=False),
+        )
+
+        self.C = C
+        self.k = k
+
+    def __call__(self, x):
+        """AutoEncoder"""
+        mu, ln_var = self.encode(x)
+        batchsize = len(mu.data)
+        # reconstruction loss
+        z = F.gaussian(mu, ln_var)
+        outputs_mu, outputs_sigma_2 = self.decode(z)
+        
+        m_vae_loss = (F.flatten(x) - F.flatten(outputs_mu))**2 / F.flatten(outputs_sigma_2)
+        m_vae_loss = 0.5 * F.sum(m_vae_loss)
+
+        a_vae_loss = F.log(2 * 3.14 * F.flatten(outputs_sigma_2))
+        a_vae_loss = 0.5 * F.sum(a_vae_loss)
+
+        kl_loss = 1 + ln_var - F.square(mu) - F.exp(ln_var)
+        kl_loss = F.sum(kl_loss, axis=-1)
+        kl_loss *= -0.5
+
+        loss = F.mean(kl_loss + m_vae_loss + a_vae_loss)
+        
+        chainer.report({'loss': loss}, self)
+        return loss
+
+    def encode(self, x):
+        # print("=== encoder ===")
+        # print(x.shape)
+        h = F.leaky_relu(self.e_bn0(self.e_c0(x)), slope=0.2)
+        # print(h.shape)
+        h = F.leaky_relu(self.e_bn1(self.e_c1(h)), slope=0.2)
+        # print(h.shape)
+        h = F.leaky_relu(self.e_bn2(self.e_c2(h)), slope=0.2)
+        # print(h.shape)
+        h = F.leaky_relu(self.e_bn3(self.e_c3(h)), slope=0.2)
+        # print(h.shape)
+        mu = self.z_mu(h)
+        ln_var = self.z_ln_var(h)
+        # print(mu.shape)
+        return mu, ln_var
+
+    def decode(self, z):
+        # print("=== decoder ===")
+        # print(z.shape)
+        h = F.relu(self.d_c0(z))
+        # print(h.shape)
+        h = F.reshape(h, (h.shape[0], 128, 1, 1))
+        # print(h.shape)
+        h = F.relu(self.d_bn0(self.d_c1(h)))
+        # print(h.shape)
+        h = F.relu(self.d_bn1(self.d_c2(h)))
+        # print(h.shape)
+        h = F.relu(self.d_bn2(self.d_c3(h)))
+        # print(h.shape)
+        x1 = F.sigmoid(self.d_bnx1(self.d_x1(h)))
+        x2 = F.sigmoid(self.d_bnx2(self.d_x2(h)))
+        # print(f"{x1.shape}, {x2.shape}")
+        return x1, x2
+
+    def get_loss_func(self, C=1.0, k=1, train=True):
+        """Get loss function of VAE.
+        The loss value is equal to ELBO (Evidence Lower Bound)
+        multiplied by -1.
+        Args:
+            C (int): Usually this is 1.0. Can be changed to control the
+                second term of ELBO bound, which works as regularization.
+            k (int): Number of Monte Carlo samples used in encoded vector.
+            train (bool): If true loss_function is used for training.
+        """
+
+    def lf(self, x):
+        """AutoEncoder"""
+        mu, ln_var = self.encode(x)
+        batchsize = len(mu.data)
+        # reconstruction loss
+        z = F.gaussian(mu, ln_var)
+        outputs_mu, outputs_sigma_2 = self.decode(z)
+        
+        m_vae_loss = (F.flatten(x) - F.flatten(outputs_mu))**2 / F.flatten(outputs_sigma_2)
+        m_vae_loss = 0.5 * F.sum(m_vae_loss)
+
+        a_vae_loss = F.log(2 * 3.14 * F.flatten(outputs_sigma_2))
+        a_vae_loss = 0.5 * F.sum(a_vae_loss)
+
+        kl_loss = 1 + ln_var - F.square(mu) - F.exp(ln_var)
+        kl_loss = F.sum(kl_loss, axis=-1)
+        kl_loss *= -0.5
+
+        self.loss = F.mean(kl_loss + m_vae_loss + a_vae_loss)
+
+        return self.loss
---
+++ train.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+"""Chainer example: train a VAE on MNIST
+"""
+from __future__ import print_function
+import argparse
+import os
+
+import matplotlib
+# Disable interactive backend
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+import numpy as np
+import pandas as pd
+import six
+
+import chainer
+from chainer import cuda, training
+from chainer import training
+from chainer.training import extensions, Trainer
+import chainer.functions as F
+
+import cvae_net
+
+import os
+import PIL
+from PIL import Image, ImageFilter
+
+from tqdm import tqdm
+
+# Size of cutting image
+CutSize = 16
+# Path of dataset folder
+Path = "images/"
+
+# Make datasets from image
+def road_data():
+    print("Loading datasets...")
+    d = pd.read_csv(f"{Path}list.txt", header=None)
+    train = []
+    with tqdm(total=len(list(d.iterrows()))) as pbar:
+        for i, data in tqdm(d.itertuples()):
+            pil_img = PIL.Image.open(Path + data)
+            img = np.array(pil_img.resize((240,180)).convert("L"))
+            imarray = img.astype(np.float32) / 255.
+            train.append((imarray))
+            pbar.update(1)
+    train = np.array(train)
+    train = train.reshape(1000, 180, 240, 1)
+    print(f"Done, {train.shape}")
+    
+    return train
+
+def cut_img(x, number, width = CutSize, height = CutSize):
+    print("cutting images ...")
+    x_out = []
+    x_shape = x.shape
+    
+    for i in range(number):
+        shape_0 = np.random.randint(0,x_shape[0])
+        shape_1 = np.random.randint(0,x_shape[1]-height)
+        shape_2 = np.random.randint(0,x_shape[2]-width)
+        temp = x[shape_0, shape_1:shape_1+height, shape_2:shape_2+width, 0]
+        x_out.append(temp.reshape((height, width, x_shape[3])))
+    print("Complete.")
+    out = np.array(x_out)
+
+    return out
+
+def save_img(x_anomaly, img_anomaly, name):
+    path = 'results/'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    img_max = img_anomaly.max()
+    img_min = img_anomaly.min()
+    img_anomaly = (img_anomaly-img_min)/(img_max-img_min) * 9 + 1
+
+    plt.subplot(2,1,1)
+    print(img_anomaly.shape)
+    plt.imshow(x_anomaly[0,:,:], cmap='gray')
+    plt.axis('off')
+    plt.colorbar()
+
+    plt.subplot(2,1,2)
+    plt.imshow(img_anomaly[0,0,:,:], cmap='Blues',norm=colors.LogNorm())
+    plt.axis('off')
+    plt.colorbar()
+
+    plt.clim(1, 10)
+
+    plt.savefig(path + name +".png")
+
+    plt.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the optimization from snapshot')
+    parser.add_argument('--gpu', '-g', default=-1, type=int,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--epoch', '-e', default=20, type=int,
+                        help='number of epochs to learn')
+    parser.add_argument('--dimz', '-z', default=80, type=int,
+                        help='dimention of encoded vector')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='learning minibatch size')
+    parser.add_argument('--test', action='store_true',
+                        help='Use tiny datasets for quick tests')
+    parser.add_argument('--out', '-o', type=str, default='./result/',
+                        help='dir to save snapshots.')
+    parser.add_argument('--interval', '-i', type=int, default=1, help='interval of save images.')
+    parser.add_argument
+    args = parser.parse_args()
+
+    batchsize = args.batchsize
+    n_epoch = args.epoch
+    n_latent = args.dimz
+
+    print('GPU: {}'.format(args.gpu))
+    print('# dim z: {}'.format(args.dimz))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# epoch: {}'.format(args.epoch))
+    print('')
+
+    model = cvae_net.CVAE(1, 16, n_latent, 16)
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        model.to_gpu()
+    xp = np if args.gpu < 0 else cuda.cupy
+
+    # Setup optimizer
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+    
+    # Prepare Dataset 
+    data = road_data().astype(np.float32)
+    # Prepare test(not separated) images
+    test_img = data[950:1000,:,:,:]
+    test_img = np.transpose(test_img, (0,3,1,2))
+    # Prepare separated images
+    data = cut_img(data, 100000)
+    data = np.transpose(data, (0,3,1,2))
+    train, test = chainer.datasets.split_dataset(data, 80000)
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+        repeat=False, shuffle=False)
+    # Set up a trainer
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+        # Evaluate the model with the test dataset for each epoch
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+
+    # Dump a computational graph from 'loss' variable at the first iteration
+    # The "main" refers to the target link of the "main" optimizer.
+    if not os.path.exists(os.path.join(args.out, 'cg.dot')):
+        print('dump computational graph of `main/loss`')
+        trainer.extend(extensions.dump_graph('main/loss'))
+
+    # Take a snapshot at each epoch
+    trainer.extend(extensions.snapshot(filename='snapshot_epoch_{.updater.epoch}'), trigger=(args.interval, 'epoch'))
+
+    # Write a log of evaluation statistics for each epoch
+    trainer.extend(extensions.LogReport())
+    # if you want to output different log files epoch by epoch,
+    # use below statement.
+    #trainer.extend(extensions.LogReport(log_name='log_'+'{epoch}'))
+
+    # Print selected entries of the log to stdout
+    # Here "main" refers to the target link of the "main" optimizer again, and
+    # "validation" refers to the default name of the Evaluator extension.
+    # Entries other than 'epoch' are reported by the Classifier link, called by
+    # either the updater or the evaluator.
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss']))
+
+    # Print a progress bar to stdout
+    trainer.extend(extensions.ProgressBar())
+
+    # if you want to show the result images epoch by epoch,
+    # use the extension below.
+    @training.make_extension(trigger=(args.interval, 'epoch'))
+    def save_images(trainer):
+        height = 16
+        width = 16
+        move = 2
+        test_ind = [3, 2, 1, 18, 4, 8, 11, 17, 61]
+        x = cuda.cupy.asarray(test_img)[test_ind, :]
+        for index, image in enumerate(x):
+            x_batch = []
+            img_anomaly = xp.zeros((1, 1, image.shape[1], image.shape[2]))
+            # Making batch of separated test image
+            for i in tqdm(range(int((image.shape[1]-height)/move)+1)):
+                for j in range(int((image.shape[2]-width)/move)+1):
+                    x_sub = image[0, i*move:i*move+height, j*move:j*move+width]
+                    x_sub = x_sub.reshape(1, height, width)
+                    x_batch.append(x_sub)
+            x_batch = xp.asarray(x_batch, dtype=xp.float32)
+            print(f"{x_batch.shape}")
+            # Encoding
+            x_batch = chainer.Variable(x_batch)
+            mu, ln_var = model.encode(x_batch)
+            z = F.gaussian(mu, ln_var)      
+            mu, sigma = model.decode(z)
+            # Evaluate anormaly to calculate loss
+            batch_index = 0
+            for i in tqdm(range(int((image.shape[1]-height)/move)+1)):
+                for j in range(int((image.shape[2]-width)/move)+1):  
+                    loss = 0.5 * (x_batch[batch_index,0,:,:] - mu[batch_index,0,:,:])**2 / sigma[batch_index,0,:,:]
+                    loss = F.sum(loss)
+                    img_anomaly[0, 0, i*move:i*move+height, j*move:j*move+width] += loss.data
+                    batch_index += 1
+            # Saving
+            save_img(chainer.cuda.to_cpu(image), chainer.cuda.to_cpu(img_anomaly), f"{index}_{trainer.updater.epoch}")
+    
+    trainer.extend(save_images)
+
+    if args.resume:
+        # Resume from a snapshot
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    # Run the training
+    trainer.run()
+
+if __name__ == '__main__':
+    main()
